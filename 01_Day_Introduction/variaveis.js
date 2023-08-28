206030c5dae475756549265fa4b3919cdfb634a7
WoodyGuerra <125077186+WoodyGuerra@users.noreply.github.com>
2023-08-27 23:48:07 -0300

    dia 1

---
+++ 01_Day_Introduction/variaveis.js
@@ -0,0 +1,14 @@
+let firstName = 'Jennifer'
+let lastName = 'Raquel Aragão'
+let country = 'Finland'   
+let city = 'Sousa'        
+let age = 46                 
+let isMarried = true
+
+const gravity = 9.81
+const boilingPoint = 100     
+const PI = 3.14   
+
+let name = 'Jennifer',     
+  job = 'Nada',
+  live = 'Finland'
