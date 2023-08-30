4bb5989276e3d62de78361d58c8932a3c5c89b3f
WoodyGuerra <125077186+WoodyGuerra@users.noreply.github.com>
2023-08-30 17:15:30 -0300

    dia 3 level 3

---
+++ 03_booleans_operators_date/exerciseLevel3.js
@@ -0,0 +1,7 @@
+let date = new Date();
+let YYYMMDD = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
+let DDMMYYYY = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()} ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
+let DdMmYYy = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
+console.log(YYYMMDD);
+console.log(DDMMYYYY);
+console.log(DdMmYYy);
