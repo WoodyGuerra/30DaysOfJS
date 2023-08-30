a9b0cf55a89ce6d552d21d1a06c6efeed69f46a5
WoodyGuerra <125077186+WoodyGuerra@users.noreply.github.com>
2023-08-30 12:26:09 -0300

    dia 5 level 1 completo

---
+++ 05_Day_Arrays/exerciseLevel1.js
@@ -0,0 +1,50 @@
+// const array = [];
+// const ar = [1,2,3,4,5,6]
+// console.log(ar.length)
+// console.log(`O primeiro item: ${ar[0]} o item do meio: ${ar[Math.floor(ar.length/2)]} o ultimo item: ${ar[ar.length-1]}`)
+// const mixedDataTypes = [1,'asd',true,[1,2,3],{nome:'joao'}]
+// console.log(mixedDataTypes)
+// const itCompanies = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"]
+// console.log(itCompanies)
+// console.log(itCompanies.length)
+// console.log(`O primeiro item: ${itCompanies[0]} o item do meio: ${itCompanies[Math.floor(itCompanies.length/2)]} o ultimo item: ${itCompanies[itCompanies.length-1]}`)
+// console.log(itCompanies.toString().toUpperCase().split(','))
+// console.log(itCompanies)
+// if(itCompanies.includes('Apple'))
+// {
+//     console.log('Apple')
+// }
+// else
+// {
+//     console.log('Não esta')
+// }
+// function oo(value)
+// {
+//     let count = 0;
+//     const o2 = value.split('');
+//     for(let i = 0;i < o2.length;i++)
+//     {
+//         if(o2[i] == 'o')
+//         {
+//             count++;
+//         }
+//     }
+//     if(count >= 2){
+//         console.log(true)
+//     }
+//     else
+//     {
+//         console.log(false)
+//     }   
+// }
+// itCompanies.forEach(oo);
+// console.log(itCompanies.sort());
+// console.log(itCompanies.reverse());
+// console.log(itCompanies.slice(0,3))
+// console.log(itCompanies.slice(4))
+// itCompanies.shift()
+// itCompanies.splice(Math.floor(ar.length/2),1)
+// itCompanies.pop()
+// console.log(itCompanies)
+// itCompanies.splice(0)
+// console.log(itCompanies)
